bd17a29d6b686423bfc8ba1a15cf1a320d01b122
Maksim <mashnert@mail.ru>
2022-11-23 21:28:10 +0300

    initial commit

---
+++ src/main/java/com/gergia/me/coffeeproject/CoffeeProjectApplication.java
@@ -0,0 +1,14 @@
+package com.gergia.me.coffeeproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CoffeeProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CoffeeProjectApplication.class, args);
+    }
+
+}
+
