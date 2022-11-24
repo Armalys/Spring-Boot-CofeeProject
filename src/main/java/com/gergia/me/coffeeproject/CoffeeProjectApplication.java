2257b4daed0330bbdbb219be69b169d3b8afcdc5
Maksim <mashnert@mail.ru>
2022-11-24 23:17:05 +0300

    add Droids

--- src/main/java/com/gergia/me/coffeeproject/CoffeeProjectApplication.java
+++ src/main/java/com/gergia/me/coffeeproject/CoffeeProjectApplication.java
@@ -1,8 +1,11 @@
 package com.gergia.me.coffeeproject;
 
+import com.gergia.me.coffeeproject.model.Droid;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.context.properties.ConfigurationProperties;
 import org.springframework.boot.context.properties.ConfigurationPropertiesScan;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 @ConfigurationPropertiesScan
@@ -12,5 +15,10 @@ public class CoffeeProjectApplication {
         SpringApplication.run(CoffeeProjectApplication.class, args);
     }
 
+    @Bean
+    @ConfigurationProperties(prefix = "droid")
+    Droid createDroid() {
+        return new Droid();
+    }
 }
 
---
+++ src/main/java/com/gergia/me/coffeeproject/model/Droid.java
@@ -0,0 +1,21 @@
+package com.gergia.me.coffeeproject.model;
+
+public class Droid {
+    private String id, description;
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getId() {
+        return id;
+    }
+
+    public void setId(String id) {
+        this.id = id;
+    }
+}
